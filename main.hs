3a2adef6c8f4949b7893ab23c4756e2d8121c1b4
JT <jbowler2400@gmail.com>
2016-11-25 12:43:08 -0500

    Clean up some junk

--- main.hs
+++ main.hs
@@ -5,9 +5,6 @@ import Token
 
 main = do
   let tokens = lexer "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3"
-  -- let tokens = lexer "1 / 5 + 1"
-  print tokens
   let rpnTokens = convertToRpn tokens
-  print rpnTokens
   let results = solveRpnTokens rpnTokens
   print results
