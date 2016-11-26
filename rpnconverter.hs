8c2fca412bfaf58051c5cb0795a06badc9e04767
JT <jbowler2400@gmail.com>
2016-11-25 20:49:39 -0800

    Simplify some code and give names to variables

--- rpnconverter.hs
+++ rpnconverter.hs
@@ -6,14 +6,14 @@ convertToRpn tokens = let (remainingOps, output) = break tokens ([], []) in outp
   where
     break [] tuples = tuples
     break (t@(FloatToken _):ts) (opStack, outputStack) = break ts (opStack, outputStack ++ [t])
-    break (t@LeftParen:ts) (opStack, outputStack) = break ts (t:opStack, outputStack)
-    break (t@RightParen:ts) (opStack, outputStack) =
+    break (LeftParen:ts) (opStack, outputStack) = break ts (LeftParen:opStack, outputStack)
+    break (RightParen:ts) (opStack, outputStack) =
       let (toPop, (_:toRemain)) = span (not . isLeftParen) opStack
         in break ts (toRemain, outputStack ++ (reverse toPop))
         where isLeftParen LeftParen = True
               isLeftParen _ = False
     break (o1@(OpToken _):ts) (opStack, outputStack) =
-      let (a, b) = span (shouldPop o1) opStack
-        in break ts (o1:b, outputStack ++ (reverse a))
+      let (poppedTokens, unpoppedTokens) = span (shouldPop o1) opStack
+        in break ts (o1:unpoppedTokens, outputStack ++ (reverse poppedTokens))
         where shouldPop o1 o2@(OpToken _) = (isLeftAssociative o1 && o1 <= o2) || (isRightAssociative o1 && o1 < o2)
               shouldPop o1 o2 = False
